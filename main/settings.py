40a5bbac49bbf7ec7fdbbe7af662fc3b4d4c1c67
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-16 14:23:51 +0530

    feat(main/settings.py):database changed to mysql

--- main/settings.py
+++ main/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "agriculture",
+        "USER": "root",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
