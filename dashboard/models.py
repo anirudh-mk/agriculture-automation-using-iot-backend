0bf7bf155bf408a79172ebf2dede2a8bc1c4a487
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-16 14:53:54 +0530

    feat(dashboard/models.py): auth user edited to user

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_superuser = None
+    is_staff = None
+
+    qr_code = models.CharField(
+        unique=True,
+        max_length=36,
+        default=uuid.uuid4()
+    )
