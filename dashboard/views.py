b7c026e4e500aec3bace73a436f36ac98ff42853
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-17 15:24:26 +0530

    feat(dashboard/urls): urlpattern added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
 from .models import User
 from utils.response import CustomResponse
+from rest_framework.permissions import AllowAny
 
 
 class CreateUserAPI(APIView):
+    permission_classes = (AllowAny,)
+
     def post(self, request):
+
         first_name = request.data.get('first_name')
         last_name = request.data.get('last_name')
         email = request.data.get('email')
-        username = request.data.get('user_name')
+        username = request.data.get('username')
         password = request.data.get('password')
         confirm_password = request.data.get('confirm_password')
 
@@ -17,17 +21,22 @@ class CreateUserAPI(APIView):
                 general_message="user name already exists"
             ).get_failure_response()
 
+        if User.objects.filter(email=email).exists():
+            return CustomResponse(
+                general_message="email already registered"
+            ).get_failure_response()
+
         if password != confirm_password:
             return CustomResponse(
                 general_message="password and confirm password dose not match"
             ).get_failure_response()
 
         user = User.objects.create_user(
-            first_name=first_name,
-            last_name=last_name,
             username=username,
             password=password,
-            email=email
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
         )
 
         user.save()
