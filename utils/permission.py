a652cff874f1fe76eaed26f1be777a11ddbfde67
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-17 18:11:40 +0530

    feat(utils/permission.py):token generate added

--- utils/permission.py
+++ utils/permission.py
@@ -54,3 +54,25 @@ class JWTUtils:
         return user_id
 
 
+class TokenGenerate:
+    def generate(self, user):
+        if user is not None:
+            access_expiry_time = datetime.now() + timedelta(seconds=1800)  # 30 minutes
+            access_expiry = access_expiry_time.strftime("%d/%m/%Y %H:%M:%S")
+
+            access_token = jwt.encode(
+                {'id': user.id, 'expiry': access_expiry,
+                 'tokenType': 'access'},
+                'SEDKLK23D@LK323#@!2',
+                algorithm="HS256")
+
+            refresh_expiry_time = datetime.now() + timedelta(seconds=259200)  # 3 days
+            refresh_expiry = refresh_expiry_time.strftime("%d/%m/%Y %H:%M:%S")
+            refresh_token = jwt.encode(
+                {'id': user.id, 'expiry': refresh_expiry, 'tokenType': 'refresh'},
+                'SEDKLK23D@LK323#@!2',
+                algorithm="HS256")
+            auth = {'accessToken': access_token, 'refreshToken': refresh_token, 'expiry': access_expiry}
+        else:
+            auth = None
+        return auth
