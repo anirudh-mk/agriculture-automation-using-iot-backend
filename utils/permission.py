9c3f1e57662d8fc6c6b4010d6b6c9cfe188b9a9b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-17 18:10:38 +0530

    feat(utils/permission.py):custom permission added

--- utils/permission.py
+++ utils/permission.py
@@ -15,3 +15,30 @@ class CustomException(ValidationError):
         self.status_code = status_code
 
 
+class CustomizePermission(BasePermission):
+    def authenticate(self, request):
+        try:
+            token = authentication.get_authorization_header(request).decode("utf-8").split()
+            if token[0] != "Bearer" and len(token) != 2:
+                exception_message = {"hasError": True, "message": "Invalid token", "statusCode": 1000}
+                raise CustomException(exception_message)
+            return self._authenticate_credentials(request, token[1])
+        except Exception:
+            exception_message = {"hasError": True, "message": "Invalid token", "statusCode": 1000}
+            raise CustomException(exception_message)
+
+    def _authenticate_credentials(self, request, token):
+        payload = jwt.decode(token, 'SEDKLK23D@LK323#@!2', algorithms=["HS256"], verify=True)
+
+        id = payload.get("id", None)
+        expiry = payload.get("expiry", None)
+        if id and expiry:
+            # if expiry check expiry and return Token Expired
+            user = User.objects.filter(id=id)
+            if user.exists():
+                return user.first(), payload
+            else:
+                pass
+        return None, payload
+
+
